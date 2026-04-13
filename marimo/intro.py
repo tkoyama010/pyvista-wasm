805259fe0555b3a08d9db6182fba159059a57a0e
Tetsuo Koyama <tkoyama010@gmail.com>
2026-04-13 16:08:20 +0900

    fix: use srcdoc iframe in mo.Html to avoid worker DOM access limitations

--- marimo/intro.py
+++ marimo/intro.py
@@ -22,35 +22,16 @@ def _(micropip):
 
 @app.cell
 def _(mo, pv):
-    import js
-    from pyodide.ffi import create_proxy
-
     plotter = pv.Plotter()
     plotter.add_mesh(pv.Sphere(), color="red")
     html_content = plotter.generate_standalone_html()
 
-    container_id = "vtk-wasm-container"
-
-    def inject_vtk():
-        container = js.document.getElementById(container_id)
-        if container is None:
-            return
-        iframe = js.document.createElement("iframe")
-        iframe.srcdoc = html_content
-        iframe.style.cssText = "width:600px;height:400px;min-height:400px;border:2px solid #333;"
-        iframe.sandbox = "allow-scripts"
-        container.innerHTML = ""
-        container.appendChild(iframe)
-
-    proxy = create_proxy(inject_vtk)
-    js.window.setTimeout(proxy, 500)
+    escaped = html_content.replace("&", "&amp;").replace('"', "&quot;")
 
     return mo.Html(
-        f'<div id="{container_id}" '
-        'style="width:600px;height:400px;background:#f0f0f0;'
-        'display:flex;align-items:center;justify-content:center;">'
-        "<p>Loading VTK.wasm...</p>"
-        "</div>",
+        f'<iframe srcdoc="{escaped}" '
+        'style="width:600px;height:400px;min-height:400px;border:2px solid #333;" '
+        'sandbox="allow-scripts allow-same-origin"></iframe>'
     )
 
 
