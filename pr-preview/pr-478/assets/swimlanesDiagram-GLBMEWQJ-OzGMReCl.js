a63ba61d1c256ab1a130a7bc1a87c6c3d255f283
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-08-08 07:09:44 +0000

    Remove preview for PR 478 🛬

--- pr-preview/pr-478/assets/swimlanesDiagram-GLBMEWQJ-OzGMReCl.js
+++
@@ -1,8 +0,0 @@
-import{r as e}from"./chunk-PTVI3W5X-DVfdoxm6.js";import"./chunk-FO5PYUIK-qrvO4cW9.js";import"./chunk-NEZ6ONQZ-sSTMov6C.js";import"./chunk-PWCFYZI5-CqdFz4Y2.js";import"./chunk-PLCLPJVV-rGjVDjLw.js";import"./chunk-XXV7AQMY-CJkvkGFp.js";import"./chunk-IPM4HZQ6-CQmHNgJe.js";import"./chunk-OX6ZKUVE-GPVuJDeO.js";import"./chunk-YV36FAQU-BhswPCUR.js";import"./chunk-AQ2D6KKF-CBp11d8e.js";import"./chunk-2V3FENDJ-4aaXXoNp.js";import"./chunk-DKH7L6O3-pKwK4i8-.js";import"./chunk-RTI7CJYH-CrTK6TD7.js";import"./chunk-MPM6TOF7-Cac2BFof.js";import"./chunk-STSWKLMO-vwIQ_gDp.js";import"./chunk-ZICDAICJ-DFq67tq6.js";import"./chunk-HPLX5OYV-D7oZrNPZ.js";import"./chunk-BYAF6FXG-DiDvaCrt.js";import{r as t,t as n}from"./chunk-MX3EQCGM-CUlPhxSc.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
-  .swimlane.cluster rect {
-    stroke: ${e.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,`getStyles`)});export{r as diagram};
