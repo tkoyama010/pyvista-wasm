5199261a70828a6e4f041051e1eba9e76274a219
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-08-08 04:44:44 +0000

    deploy: 1b84ccbffa0858f7f28ba0146c62e9de641bc411

---
+++ slides/assets/swimlanesDiagram-GLBMEWQJ-DDj6HPbj.js
@@ -0,0 +1,8 @@
+import{r as e}from"./chunk-PTVI3W5X-DVfdoxm6.js";import"./chunk-FO5PYUIK-qrvO4cW9.js";import"./chunk-NEZ6ONQZ-CejgDs2k.js";import"./chunk-PWCFYZI5-CqdFz4Y2.js";import"./chunk-PLCLPJVV-rGjVDjLw.js";import"./chunk-XXV7AQMY-En78B6s0.js";import"./chunk-IPM4HZQ6-CQmHNgJe.js";import"./chunk-OX6ZKUVE-CuoafBXT.js";import"./chunk-YV36FAQU-BaHDWBH5.js";import"./chunk-AQ2D6KKF-a3MwNO1W.js";import"./chunk-2V3FENDJ-OHHg9MmU.js";import"./chunk-DKH7L6O3-BN4jKD5r.js";import"./chunk-RTI7CJYH-CrTK6TD7.js";import"./chunk-MPM6TOF7-DrZrz-J4.js";import"./chunk-STSWKLMO-DnQBRVVw.js";import"./chunk-ZICDAICJ-DkQWXRxL.js";import"./chunk-HPLX5OYV-D7oZrNPZ.js";import"./chunk-BYAF6FXG-B8pGew2T.js";import{r as t,t as n}from"./chunk-MX3EQCGM-CUqsWgaK.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
