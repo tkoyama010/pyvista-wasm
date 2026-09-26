e15fe266a8253f3d5b2fe8c5265eacec24bcb94e
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-09-26 16:46:52 +0000

    Deploy preview for PR 707 🛫

---
+++ pr-preview/pr-707/assets/slidev/full-uI_JU_sj.js
@@ -0,0 +1,1 @@
+import{H as e,x as t,z as n}from"../modules/shiki-Cd3StX6p.js";import{t as r}from"../_plugin-vue_export-helper-BDNMzG2s.js";var i={},a={class:`slidev-layout full w-full h-full`};function o(r,i){return n(),t(`div`,a,[e(r.$slots,`default`,{class:`w-full h-full`})])}var s=r(i,[[`render`,o]]);export{s as t};
